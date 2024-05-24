89f62d638b057cffd491a89e4470d5c351c14180
SithLor <84148006+SithLor@users.noreply.github.com>
2024-05-24 17:36:48 +0000

    fuck no_std ihml

--- efi_app/src/main.rs
+++ efi_app/src/main.rs
@@ -1,47 +1,65 @@
 #![no_main]
 #![no_std]
+//this line 37,func get_firmware_vendor,
+#![feature(vec_into_raw_parts)]
+#![feature(ascii_char)]
 
-use uefi::{prelude::*, table::{runtime::{ResetType, Time, TimeCapabilities}}};
 use log::info;
+use uefi::{
+    prelude::*, table::runtime::{ResetType, Time, TimeCapabilities}, CString16, Guid
+};
 
-fn Shutdown(system_table: &SystemTable<Boot>){
-    system_table.runtime_services().reset(ResetType::SHUTDOWN, Status::SUCCESS, None);
-}
 //https://rust-osdev.github.io/uefi-rs/HEAD/concepts/variables.html
 //https://docs.rs/uefi/latest/uefi/table/runtime/struct.ResetType.html#associatedconstant.SHUTDOWN
+//https://wiki.osdev.org/Main_Page
+
+
+
+fn util_shutdown(system_table: &SystemTable<Boot>) {
+    system_table
+        .runtime_services()
+        .reset(ResetType::SHUTDOWN, Status::SUCCESS, None);
+}
+fn get_firmware_vendor(system_table: &SystemTable<Boot>) -> &str { 
+    let _firmware_vendor: CString16 = CString16::from(system_table.firmware_vendor()).clone();
+
+    if (_firmware_vendor.is_empty()){return "Unknown";}
+
+    if _firmware_vendor.is_ascii(){
+        let length = _firmware_vendor.to_u16_slice_with_nul().len();
+        let mut final_data:&[&str] = &["";];
+        //u16 slice with nul  to   
+
+
+        return final_data
+    } else {
+        return "Unknown";
+    }
+
+ 
+}
+fn get_firmware_revision(system_table: &SystemTable<Boot>) -> u32{
+    let firmware_revision = system_table.firmware_revision();
+    return firmware_revision;
+}
+fn get_firmware_revision_str(system_table: &SystemTable<Boot>) -> &str{
+    let firmware_revision = system_table.firmware_revision();
+    let g: Option<&str> = format_args!("{}.{}", firmware_revision >> 16, firmware_revision & 0xffff).as_str();
+    match g {
+        Some(g) => return g,
+        None => "0.0",
+    }
+    
+}
+fn get_uefi_revision(system_table: &SystemTable<Boot>) -> uefi::table::Revision{
+    system_table.uefi_revision()
+}
 #[entry]
 fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi::helpers::init(&mut system_table).unwrap();
 
-    info!("Hello, UEFI!");
-    
+    info!("Firmware Vendor: {}", get_firmware_vendor(&system_table));
 
-
-    let firmware_rev: u32 = system_table.firmware_revision();
-    let uefi_rev: uefi::table::Revision = system_table.uefi_revision();
-
-
-    let time: Result<(Time, TimeCapabilities), uefi::Error> = system_table.runtime_services().get_time_and_caps();
-    match time {
-        Ok((time, capabilities)) => {
-            info!("Time: {}.{}.{} {}:{}:{}", time.year(), time.month(), time.day(), time.hour(), time.minute(), time.second());
-            info!("Time capabilities: {:?}", capabilities);
-        }
-        Err(e) => {
-            info!("Failed to get time: {:?}", e);
-        }
-    }
-    info!("Firmware revision: {}.{}", firmware_rev >> 16, firmware_rev & 0xffff);
-    info!("UEFI revision: {}.{}", uefi_rev.major(), uefi_rev.minor());
-
-    let bios_info = uefi::proto::device_path::DevicePath:
-    //let mut c = 0;
-    //while c < 4 {
-    //    if (c == 0){
-    //        info!("CPU: {}", 1);
-    //    }
-    //    
-    //}
-
-    system_table.runtime_services().reset(ResetType::SHUTDOWN, Status::SUCCESS, None);
+    util_shutdown(&system_table);
+    return Status::SUCCESS;
 }
---
+++ efi_app/target/debug/deps/uefi_app-ec18b9f13603100a.d
@@ -0,0 +1,5 @@
+/workspaces/NativeShell_rs/efi_app/target/debug/deps/uefi_app-ec18b9f13603100a: src/main.rs
+
+/workspaces/NativeShell_rs/efi_app/target/debug/deps/uefi_app-ec18b9f13603100a.d: src/main.rs
+
+src/main.rs:
