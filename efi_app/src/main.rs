ac1066b87158173aa7455117e01374d696d05d01
SithLor <84148006+SithLor@users.noreply.github.com>
2024-05-29 16:00:04 +0000

    work https

--- efi_app/src/main.rs
+++ efi_app/src/main.rs
@@ -72,28 +72,10 @@ fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     let mut handles = [MaybeUninit::<Handle>::uninit(); 1];
     let locate_handle_result = bs.locate_handle(SearchType::ByProtocol(&gop_guid), Some(&mut handles));
     
-    if let Ok(num_handles) = locate_handle_result {
-        if num_handles > 0 {
-            let handle: Handle = unsafe { handles[0].assume_init() };
-            let params: OpenProtocolParams = OpenProtocolParams {
-                handle,
-                agent: _handle, // _handle is the handle of the application
-                controller: None,
-            };
-            let open_protocol_result = unsafe { bs.open_protocol::<GraphicsOutput>(params, OpenProtocolAttributes::Exclusive) };
-            match open_protocol_result {
-                Ok(gop) => {
-                    // Use gop here
-                },
-                Err(_) => {
-                    info!("Unable to locate GOP");
-                }
-            };
-        } else  {
-            info!("No GOP handles found");
-        }
-    } else {
-        info!("Unable to locate handles");
-    }
+    //ping google https serice 
+    
+
+
+    system_table.boot_services().stall(10_000_000);
     Status::SUCCESS
 }
