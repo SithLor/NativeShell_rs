519557129ee9f45463fb4f9380b90c0b52ff61b3
SithLor <84148006+SithLor@users.noreply.github.com>
2024-05-30 15:29:10 +0000

    adding linux loader

--- efi_app/src/main.rs
+++ efi_app/src/main.rs
@@ -50,6 +50,26 @@ fn get_uefi_revision(system_table: &SystemTable<Boot>) -> uefi::table::Revision 
 }
 
 mod chip_black_and_white;
+fn chip(system_table: &SystemTable<Boot>){
+    for i in 1..=8 {
+        match i {
+            1 => info!("{}", chip_black_and_white::ASCII_ART_1),
+            2 => info!("{}", chip_black_and_white::ASCII_ART_2),
+            3 => info!("{}", chip_black_and_white::ASCII_ART_3),
+            4 => info!("{}", chip_black_and_white::ASCII_ART_4),
+            5 => info!("{}", chip_black_and_white::ASCII_ART_5),
+            6 => info!("{}", chip_black_and_white::ASCII_ART_6),
+            7 => info!("{}", chip_black_and_white::ASCII_ART_7),
+            8 => info!("{}", chip_black_and_white::ASCII_ART_8),
+            _ => (),
+        }
+        // Sleep for the remaining frame time
+        system_table.boot_services().stall(100000);
+        //clear the console
+        let _ = system_table.stdout().reset(false);
+    }
+
+}
 
 fn fps_to_nanoseconds(fps: u32) -> usize {
     return 1_000_000_000 / fps as usize;
@@ -58,29 +78,8 @@ fn fps_to_nanoseconds(fps: u32) -> usize {
 fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi::helpers::init(&mut system_table).unwrap();
 
-    //print to the console
 
-    //loop until the user presses a key
-    loop {
-        for i in 1..=8 {
-            match i {
-                1 => info!("{}", chip_black_and_white::ASCII_ART_1),
-                2 => info!("{}", chip_black_and_white::ASCII_ART_2),
-                3 => info!("{}", chip_black_and_white::ASCII_ART_3),
-                4 => info!("{}", chip_black_and_white::ASCII_ART_4),
-                5 => info!("{}", chip_black_and_white::ASCII_ART_5),
-                6 => info!("{}", chip_black_and_white::ASCII_ART_6),
-                7 => info!("{}", chip_black_and_white::ASCII_ART_7),
-                8 => info!("{}", chip_black_and_white::ASCII_ART_8),
-                _ => (),
-            }
-            // Sleep for the remaining frame time
-            system_table.boot_services().stall(100000);
-            //clear the console
-            let _ = system_table.stdout().reset(false);
-        }
-    }
 
-    //system_table.boot_services().stall(10_000_000);
+    system_table.boot_services().stall(10_000_000);
     Status::SUCCESS
 }
